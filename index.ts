6169452e32de18b74b7b13b131521cac037b396f
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-08-31 01:36:15 +0300

    fin

--- index.ts
+++ index.ts
@@ -1,13 +1,16 @@
 import { from } from 'rxjs';
 import { map } from 'rxjs/operators';
 
-//emit ({name: 'Joe', age: 30}, {name: 'Frank', age: 20},{name: 'Ryan', age: 50})
-const source = from([
+// https://www.learnrxjs.io/learn-rxjs/operators/transformation/map
+// Example 2: Map to single property
+
+//emit obj
+const srcObj$ = from([
   { name: 'Joe', age: 30 },
   { name: 'Frank', age: 20 },
-  { name: 'Ryan', age: 50 }
+  { name: 'Ryan', age: 50 },
 ]);
 //grab each persons name, could also use pluck for this scenario
-const example = source.pipe(map(({ name }) => name));
+const example = srcObj$.pipe(map(({ name }) => name));
+const subscribe = example.subscribe((val) => console.log(val));
 //output: "Joe","Frank","Ryan"
-const subscribe = example.subscribe(val => console.log(val));
